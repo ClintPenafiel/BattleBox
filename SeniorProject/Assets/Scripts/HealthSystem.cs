933c427316cda2d17b9efa9473358356018e0e34
Kyro Carolino <k_carolino@u.pacific.ed>
2023-11-15 04:30:51 -0800

    Updated universal health system

--- SeniorProject/Assets/Scripts/HealthSystem.cs
+++ SeniorProject/Assets/Scripts/HealthSystem.cs
@@ -1,15 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-    [SerializeField] public int maxHealth = 100;
+    //[SerializeField] public int maxHealth = 100;
+    public int maxHealth;
     private int currHealth;
     
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    public void StartingHealth(UnitType unitType)
+    {
+        switch (unitType)
+        {
+            case UnitType.Gatherer:
+                maxHealth = 20;
+                break;
+            case UnitType.Range:
+                maxHealth = 30;
+                break;
+            case UnitType.Melee:
+                maxHealth = 50;
+                break;
+            case UnitType.Tank:
+                maxHealth = 75;
+                break;
+            default:
+                maxHealth = 10;
+                break;
+        }
+
         currHealth = maxHealth;
     }
 
@@ -24,11 +50,22 @@ public class HealthSystem : MonoBehaviour
         currHealth -= dmgAmount;
         
         //If unit dies, destroy the gameObject
-        Die();
+        if (currHealth <= 0)
+        {
+            Die();
+        }
     }
 
     void Die()
     {
         Destroy(gameObject);
+    }
+
+    public enum UnitType
+    {
+        Gatherer,
+        Melee,
+        Range,
+        Tank
     }
 }
