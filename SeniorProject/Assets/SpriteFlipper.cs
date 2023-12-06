681634b434ed15871f1210fa09e2ed150dd06a22
Austin Nguyen <ankhoa1212@gmail.com>
2023-12-06 10:50:55 -0800

    add close range attackers

---
+++ SeniorProject/Assets/SpriteFlipper.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlipper : MonoBehaviour
+{
+    private Rigidbody2D rigBod2D;
+    private Transform myTransform;
+    private bool facingLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        facingLeft = true;
+        myTransform = GetComponent<Transform>();
+        rigBod2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((facingLeft && rigBod2D.velocity.x > 0) || (!facingLeft && rigBod2D.velocity.x < 0)) {
+            FlipImage();
+        }
+    }
+
+    private void FlipImage()
+    {
+        facingLeft = !facingLeft;
+        var localScale = myTransform.localScale;
+        Vector3 flippedScale = localScale;
+        flippedScale.x = localScale.x * -1;
+        localScale = flippedScale;
+        myTransform.localScale = localScale;
+    }
+}
