4d200599f79710c6c2a11150c22bd23bd8d250d6
ClintPenafiel <c_penafiel@u.pacific.edu>
2023-12-06 18:15:01 -0800

    refresh

--- SeniorProject/Assets/Scripts/HealthSystem.cs
+++ SeniorProject/Assets/Scripts/HealthSystem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 //using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-
+//
 public class HealthSystem : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
